968085a9f62d7b3f5b364d7b6587f6cc79ec5901
Saiful <saiful.islam@bjitacademy.com>
2024-11-29 16:32:07 +0600

    fix: convert to APIView

--- jobs/api/views.py
+++ jobs/api/views.py
@@ -1,21 +1,53 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 from .serializer import JobSerializer
 from .permission import IsAdminOrReadOnly
 from jobs.models import Job, Application, Category
 
 
-class JobListCreateView(generics.ListCreateAPIView):
-    serializer_class = JobSerializer
-    queryset = Job.objects.all()
+class JobListCreateView(APIView):
     permission_classes = [IsAdminOrReadOnly]
 
-    def perform_create(self, serializer):
-        serializer.save(added_by=self.request.user)
+    def get(self, request, *args, **kwargs):
+        jobs = Job.objects.all()
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = JobSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(added_by=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = JobSerializer
-    queryset = Job.objects.all()
+class JobDetailView(APIView):
     permission_classes = [IsAdminOrReadOnly]
+
+    def get(self, request, pk, *args, **kwargs):
+        job = get_object_or_404(Job, pk=pk)
+        serializer = JobSerializer(job)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, *args, **kwargs):
+        job = get_object_or_404(Job, pk=pk)
+        serializer = JobSerializer(job, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, *args, **kwargs):
+        job = get_object_or_404(Job, pk=pk)
+        serializer = JobSerializer(job, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        job = get_object_or_404(Job, pk=pk)
+        job.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
